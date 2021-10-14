bbd18598af76b56a02d54d9db2edd96f084076fc
dajhely angles <assiaxramx10@gmail.com>
2021-10-14 08:26:20 -0500

    Implementing posts/edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,19 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path(myposts: '1')
+    else
+      redirect_to edit_post_path(@post)
+    end
+  end
+
   private
 
   def post_params

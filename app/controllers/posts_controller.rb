56b5e49c0b8bda2d3a2fc8c6647493fa018fd31d
Denise Hermoza <denise.hmm91@gmail.com>
2021-10-14 15:24:56 -0500

    map in post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,13 @@ class PostsController < ApplicationController
     else
       @posts = Post.all.order("id ASC")
     end
+    @markers = @posts.geocoded.map do |post|
+      {
+        lat: post.latitude,
+        lng: post.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { post: post })
+      }
+    end
   end
 
   def show

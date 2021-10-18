35715a9c7341ae1c8b849e57d085fd2f5b1f2995
Denise Hermoza <denise.hmm91@gmail.com>
2021-10-18 16:32:07 -0500

    search refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,15 @@ class PostsController < ApplicationController
 
   def index
     if params[:query].present?
-      query = "address @@ :query OR title @@ :query"
-      @posts = Post.where(query, query: "%#{params[:query]}%").order("id ASC")
+      query = " \
+        posts.address @@ :query \
+        OR posts.title @@ :query \
+        OR pets.breed @@ :query \
+        OR pets.size @@ :query \
+        OR pets.gender @@ :query \
+        OR pets.color @@ :query \
+      "
+      @posts = Post.joins(:pet).where(query, query: "%#{params[:query]}%").order("id ASC")
     elsif params[:breed].present?
       sql_query = "pets.breed ILIKE :query"
       @posts = Post.joins(:pet).where(sql_query, query: params[:breed])
